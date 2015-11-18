2e9413f1b2324d557359c8b60400e47888e05dea
aleccool213 <moteygo@gmail.com>
2015-11-18 01:05:42 -0500

    class based view to the rescue!

--- mydjangoproject/mydjangoproject/urls.py
+++ mydjangoproject/mydjangoproject/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from storeapp.views import IndexView
+from storeapp.views import ProductIndexView
 
 
 urlpatterns = [
@@ -11,5 +11,5 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', IndexView.as_view())
+    url(r'^$', ProductIndexView.as_view())
 ]
