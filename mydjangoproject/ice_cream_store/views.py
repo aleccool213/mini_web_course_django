bd843dcd8f24935009521bba02c30f1c241dbfc9
aleccool213 <moteygo@gmail.com>
2016-01-27 21:07:29 -0500

    get rid of view

--- mydjangoproject/ice_cream_store/views.py
+++
@@ -1,9 +0,0 @@
-# Create your views here.
-from django.views.generic import ListView
-
-from ice_cream_store.models import Receipt
-
-
-class ProductIndexView(ListView):
-    template_name = "index.html"
-    model = Receipt
